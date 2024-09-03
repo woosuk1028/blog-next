1855f87fa6a0f8cd3db04e773a5a48d64b864fe1
WooSeok <gmn94@naver.com>
2024-09-03 16:00:57 +0900

    Mod Style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
 export default function Footer() {
     return (
         <footer className="text-black bg-white mt-8 py-4">
-            <div className="max-w-screen-md mx-auto px-4">
+            <div className="max-w-screen-md mx-auto px-4 gap-2 flex flex-col items-center justify-center">
+                <div className="container mx-auto text-center flex justify-center">
+                    <a target="_blank" href="https://github.com/woosuk1028">
+                        <svg fill="currentColor" viewBox="0 0 16 16" height="30" width="30"
+                             className="fill-foreground transition hover:fill-pink-600">
+                            <path
+                                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                        </svg>
+                    </a>
+                </div>
+
                 <div className="container mx-auto text-center">
-                    <p>&copy; {new Date().getFullYear()} My Blog. All rights reserved.</p>
-                    <p>
-                        <a
-                            href="https://example.com"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="hover:text-gray-400"
-                        >
-                            Visit our website
-                        </a>
-                    </p>
+                    <p>&copy; {new Date().getFullYear()} Woo Seok. All rights reserved.</p>
                 </div>
             </div>
         </footer>
